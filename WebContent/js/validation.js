59093a82b0b45ccd0a21b086908d4453b2de6328
rkdgns2994 <rkdgns2994@naver.com>
2020-02-20 15:08:42 +0900

    validation modify

--- WebContent/js/validation.js
+++ WebContent/js/validation.js
@@ -21,30 +21,6 @@
 		return /^[가-힣]{2,6}$/.test(this);
 	};
 	
-	// 주민등록번호 정규식 패턴 : 아래와 같다.
-	String.prototype.validationREGNO = function(){
-		// 1 1 1 1 1 1 - 1 1 1 1 1 1               8
-		// * * * * * *   * * * * * *
-		// 2+3+4+5+6+7 + 8+9+2+3+4+5 = 연산결과    매직넘버
-		// (11-(연산결과 % 11)) %10 = 최종값
-		// if(최종값 == 매직넘버) 유효한 주민등록번호
-		var moto = this.replace('-', '');
-		var magicNumber = moto.substr(12, 1);
-		var sequenceValue = '234567892345';
-		var sum = 0;
-		
-		for(var i=0; i<12; i++){
-			sum += moto.charAt(i) * sequenceValue.charAt(i); 
-		}
-		
-		var finalValue = (11-(sum%11))%10;
-		
-		if(finalValue == parseInt(magicNumber)){
-			return true;
-		}else{
-			return false;
-		}
-	};
 	
 	//	생년월일 정규식 패턴  
 	String.prototype.validationBIR = function(){
