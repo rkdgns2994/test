82bf45c8674c1c0df16f435ea64c2663457b947b
rkdgns2994 <rkdgns2994@naver.com>
2020-02-21 10:13:05 +0900

    validation modify

--- WebContent/js/validation.js
+++ WebContent/js/validation.js
@@ -21,7 +21,6 @@
 		return /^[가-힣]{2,6}$/.test(this);
 	};
 	
-	
 	//	생년월일 정규식 패턴  
 	String.prototype.validationBIR = function(){
 		return /^(19[0-9][0-9]|20\d{2})-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/.test(this);
